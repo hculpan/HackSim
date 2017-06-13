91786668a2aa0c4ef14d7f7aa53b12eea8af863b
Harry Culpan <harry@culpan.org>
2017-06-13 14:13:51 -0400

    Initial Commit

---
+++ hacksim/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hacksim
+//
+//  Created by Harry Culpan on 6/13/17.
+//  Copyright © 2017 Harry Culpan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
